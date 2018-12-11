8688bf6417b5b2932b3bacfe8f395015b81be296
Anthony <anton.p@qualisystems.com>
2018-12-11 16:09:32 +0200

    add abstract flow and runners modules

--- cloudshell/f5/flows/f5_autoload_flow.py
+++ cloudshell/f5/flows/f5_autoload_flow.py
@@ -1,13 +1,6 @@
 from cloudshell.devices.flows.snmp_action_flows import AutoloadFlow
-from cloudshell.f5.autoload.f5_generic_snmp_autoload import F5GenericSNMPAutoload
 
 
-class F5SnmpAutoloadFlow(AutoloadFlow):
+class AbstractF5SnmpAutoloadFlow(AutoloadFlow):
     def execute_flow(self, supported_os, shell_name, shell_type, resource_name):
-        with self._snmp_handler.get_snmp_service() as snmp_service:
-            f5_snmp_autoload = F5GenericSNMPAutoload(snmp_service,
-                                                     shell_name,
-                                                     shell_type,
-                                                     resource_name,
-                                                     self._logger)
-            return f5_snmp_autoload.discover(supported_os)
+        raise NotImplementedError("Class {} must implement method 'execute_flow'".format(type(self)))
--- cloudshell/f5/runners/f5_autoload_runner.py
+++ cloudshell/f5/runners/f5_autoload_runner.py
@@ -1,13 +1,12 @@
 from cloudshell.devices.runners.autoload_runner import AutoloadRunner
-from cloudshell.f5.flows.f5_autoload_flow import F5SnmpAutoloadFlow
 
 
-class F5AutoloadRunner(AutoloadRunner):
+class AbstractF5AutoloadRunner(AutoloadRunner):
     def __init__(self, logger, resource_config, snmp_handler):
-        super(F5AutoloadRunner, self).__init__(resource_config)
+        super(AbstractF5AutoloadRunner, self).__init__(resource_config)
         self._logger = logger
         self.snmp_handler = snmp_handler
 
     @property
     def autoload_flow(self):
-        return F5SnmpAutoloadFlow(self.snmp_handler, self._logger)
+        raise NotImplementedError("Class {} must implement property 'autoload_flow'".format(type(self)))
