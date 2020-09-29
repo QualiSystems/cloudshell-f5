417875c96507e7aa548ff10ff2cc2305a9eca35f
Anthony Poddubny <anton.p@qualisystems.com>
2020-09-29 11:28:02 +0300

    add SNMPv3 support

--- cloudshell/f5/flows/f5_enable_snmp_flow.py
+++ cloudshell/f5/flows/f5_enable_snmp_flow.py
@@ -1,6 +1,7 @@
 from cloudshell.devices.flows.cli_action_flows import EnableSnmpFlow
-from cloudshell.f5.command_actions.enable_disable_snmp_actions import EnableDisableSnmpActions
 from cloudshell.snmp.snmp_parameters import SNMPV3Parameters, SNMPV2WriteParameters
+
+from cloudshell.f5.command_actions.enable_disable_snmp_actions import SnmpV2Actions, SnmpV3Actions
 
 
 class F5EnableSnmpFlow(EnableSnmpFlow):
@@ -16,31 +17,75 @@ class F5EnableSnmpFlow(EnableSnmpFlow):
         self._create_group = create_group
 
     def execute_flow(self, snmp_parameters):
-        if hasattr(snmp_parameters, "snmp_community") and not snmp_parameters.snmp_community:
-            message = 'SNMP community cannot be empty'
-            self._logger.error(message)
-            raise Exception(self.__class__.__name__, message)
+        """
 
-        is_read_only_community = True
+        :param cloudshell.snmp.snmp_parameters.SNMPParameters snmp_parameters:
+        :return: commands output
+        """
+        with self._cli_handler.get_cli_service(self._cli_handler.config_mode) as cli_service:
+            if isinstance(snmp_parameters, SNMPV3Parameters):
+                enable_snmp = self._enable_snmp_v3
+            else:
+                enable_snmp = self._enable_snmp_v2
 
-        if isinstance(snmp_parameters, SNMPV2WriteParameters):
-            is_read_only_community = False
+            enable_snmp(cli_service=cli_service, snmp_parameters=snmp_parameters)
 
-        with self._cli_handler.get_cli_service(self._cli_handler.enable_mode) as session:
-            with session.enter_mode(self._cli_handler.config_mode) as config_session:
-                snmp_actions = EnableDisableSnmpActions(config_session, logger=self._logger)
-                if isinstance(snmp_parameters, SNMPV3Parameters):
-                    raise Exception("SNMPv3 currently is not supported")
-                else:
-                    current_snmp_community_list = snmp_actions.get_current_snmp_communities()
-                    if snmp_parameters.snmp_community not in current_snmp_community_list:
-                        result = snmp_actions.enable_snmp(snmp_parameters.snmp_community, is_read_only_community)
-                        if snmp_parameters.snmp_community not in snmp_actions.get_current_snmp_communities():
-                            self._logger.error("Failed to configure snmp community: {}".format(result))
-                            raise Exception("Failed to configure snmp parameters. Please see logs for details")
-                    current_snmp_access = snmp_actions.get_current_snmp_access_list()
-                    if "0.0.0.0/0" not in current_snmp_access:
-                        result = snmp_actions.enable_snmp_access()
-                        if "0.0.0.0/0" not in snmp_actions.get_current_snmp_access_list():
-                            self._logger.error("Failed to configure snmp access list: {}".format(result))
-                            raise Exception("Failed to configure snmp parameters. Please see logs for details")
+    def _enable_snmp_access(self, snmp_actions):
+        """
+
+        :param snmp_actions:
+        :return:
+        """
+        current_snmp_access = snmp_actions.get_current_snmp_access_list()
+
+        if "0.0.0.0/0" not in current_snmp_access:
+            result = snmp_actions.enable_snmp_access()
+
+            if "0.0.0.0/0" not in snmp_actions.get_current_snmp_access_list():
+                self._logger.error("Failed to configure snmp access list: {}".format(result))
+                raise Exception("Failed to configure snmp parameters. Please see logs for details")
+
+    def _enable_snmp_v2(self, cli_service, snmp_parameters):
+        """
+
+        :param cloudshell.cli.cli_service_impl.CliServiceImpl cli_service:
+        :param cloudshell.snmp.snmp_parameters.SNMPParameters snmp_parameters:
+        :return: commands output
+        """
+        snmp_community = snmp_parameters.snmp_community
+
+        if not snmp_community:
+            raise Exception("SNMP community can not be empty")
+
+        snmp_actions = SnmpV2Actions(cli_service=cli_service, logger=self._logger)
+
+        is_read_only_community = not isinstance(snmp_parameters, SNMPV2WriteParameters)
+        current_snmp_community_list = snmp_actions.get_current_snmp_communities()
+
+        if snmp_parameters.snmp_community not in current_snmp_community_list:
+            result = snmp_actions.enable_snmp(snmp_parameters.snmp_community, is_read_only_community)
+
+            if snmp_parameters.snmp_community not in snmp_actions.get_current_snmp_communities():
+                self._logger.error("Failed to configure snmp community: {}".format(result))
+                raise Exception("Failed to configure snmp parameters. Please see logs for details")
+
+        self._enable_snmp_access(snmp_actions=snmp_actions)
+
+    def _enable_snmp_v3(self, cli_service, snmp_parameters):
+        """
+
+        :param cloudshell.cli.cli_service_impl.CliServiceImpl cli_service:
+        :param cloudshell.snmp.snmp_parameters.SNMPParameters snmp_parameters:
+        :return: commands output
+        """
+        snmp_actions = SnmpV3Actions(cli_service=cli_service, logger=self._logger)
+
+        snmp_actions.add_snmp_user(
+            user=snmp_parameters.snmp_user,
+            password=snmp_parameters.snmp_password,
+            priv_key=snmp_parameters.snmp_private_key,
+            auth_proto=snmp_parameters.auth_protocol,
+            priv_proto=snmp_parameters.private_key_protocol
+        )
+
+        self._enable_snmp_access(snmp_actions=snmp_actions)
