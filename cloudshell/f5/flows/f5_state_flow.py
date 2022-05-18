54a95e664bc204c187d5b678d62b238395886a47
shallowfast <vitalyshelofast@gmail.com>
2022-05-18 09:21:29 +0300

    cleanup code

--- cloudshell/f5/flows/f5_state_flow.py
+++ cloudshell/f5/flows/f5_state_flow.py
@@ -1,10 +1,5 @@
 from cloudshell.shell.flows.state.basic_flow import StateFlow
 
-from cloudshell.f5.command_actions.sys_config_actions import (
-    F5SysActions,
-    F5SysConfigActions,
-)
-
 
 class F5StateFlow(StateFlow):
     pass
