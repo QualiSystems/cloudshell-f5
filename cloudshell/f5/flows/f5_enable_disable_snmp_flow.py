0934ed510c806c68d673651d8b8f385682dc5936
Yaroslav Nikonorov <yaroslav.n@quali.com>
2022-08-16 13:09:22 +0300

    Fix requirements, add typing

--- cloudshell/f5/flows/f5_enable_disable_snmp_flow.py
+++ cloudshell/f5/flows/f5_enable_disable_snmp_flow.py
@@ -1,5 +1,11 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from cloudshell.snmp.snmp_configurator import EnableDisableSnmpFlowInterface
-from cloudshell.snmp.snmp_parameters import (  # todo write ok? used to be v2write
+from cloudshell.snmp.snmp_parameters import (
+    SNMPReadParameters,
+    SNMPV3Parameters,
     SNMPWriteParameters,
 )
 
@@ -8,9 +14,24 @@ from cloudshell.f5.command_actions.enable_disable_snmp_actions import (
     SnmpV3Actions,
 )
 
+if TYPE_CHECKING:
+    from logging import Logger
+    from typing import Union
+
+    from cloudshell.cli.service.cli_service_impl import CliServiceImpl
+
+    from cloudshell.f5.cli.f5_cli_configurator import F5CliConfigurator
+
+    SnmpParams = Union[SNMPReadParameters, SNMPWriteParameters, SNMPV3Parameters]
+
 
 class F5EnableDisableSnmpFlow(EnableDisableSnmpFlowInterface):
-    def __init__(self, cli_configurator, logger, create_group=True):
+    def __init__(
+        self,
+        cli_configurator: F5CliConfigurator,
+        logger: Logger,
+        vrf_name: str = None,
+    ):
         """
         Enable Disable snmp flow.
 
@@ -20,20 +41,24 @@ class F5EnableDisableSnmpFlow(EnableDisableSnmpFlowInterface):
         """
         self._cli_configurator = cli_configurator
         self._logger = logger
-        self._create_group = create_group
+        self._create_group = True
 
-    def enable_snmp(self, snmp_parameters):
+    def enable_snmp(self, snmp_parameters: SnmpParams) -> None:
         with self._cli_configurator.config_mode_service() as cli_service:
             if snmp_parameters.version == snmp_parameters.SnmpVersion.V3:
                 self._enable_snmp_v3(cli_service, snmp_parameters)
             else:
                 self._enable_snmp(cli_service, snmp_parameters)
 
-    def _enable_snmp(self, cli_service, snmp_parameters):
+    def _enable_snmp(
+        self,
+        cli_service: CliServiceImpl,
+        snmp_parameters: SNMPReadParameters | SNMPWriteParameters,
+    ) -> None:
         """Enable SNMP v2.
 
-        :param cloudshell.cli.cli_service_impl.CliServiceImpl cli_service:
-        :param cloudshell.snmp.snmp_parameters.SNMPParameters snmp_parameters:
+        :param cli_service:
+        :param snmp_parameters:
         :return: commands output
         """
         snmp_community = snmp_parameters.snmp_community
@@ -64,7 +89,9 @@ class F5EnableDisableSnmpFlow(EnableDisableSnmpFlowInterface):
 
         self._enable_snmp_access(snmp_actions=snmp_actions)
 
-    def _enable_snmp_v3(self, cli_service, snmp_parameters):
+    def _enable_snmp_v3(
+        self, cli_service: CliServiceImpl, snmp_parameters: SNMPV3Parameters
+    ) -> None:
         """Enable SNMP v3.
 
         :param cloudshell.cli.cli_service_impl.CliServiceImpl cli_service:
@@ -77,13 +104,13 @@ class F5EnableDisableSnmpFlow(EnableDisableSnmpFlowInterface):
             user=snmp_parameters.snmp_user,
             password=snmp_parameters.snmp_password,
             priv_key=snmp_parameters.snmp_private_key,
-            auth_proto=snmp_parameters.auth_protocol,
-            priv_proto=snmp_parameters.private_key_protocol,
+            auth_proto=snmp_parameters.snmp_auth_protocol,
+            priv_proto=snmp_parameters.snmp_private_key_protocol,
         )
 
         self._enable_snmp_access(snmp_actions=snmp_actions)
 
-    def _enable_snmp_access(self, snmp_actions):
+    def _enable_snmp_access(self, snmp_actions: SnmpV2Actions | SnmpV3Actions) -> None:
         """Enable SNMP access.
 
         :param snmp_actions:
@@ -102,19 +129,22 @@ class F5EnableDisableSnmpFlow(EnableDisableSnmpFlowInterface):
                     "Failed to configure snmp parameters. Please see logs for details"
                 )
 
-    def disable_snmp(self, snmp_parameters):
+    def disable_snmp(self, snmp_parameters: SnmpParams) -> None:
         with self._cli_configurator.config_mode_service() as cli_service:
             if snmp_parameters.version == snmp_parameters.SnmpVersion.V3:
                 self._disable_snmp_v3(cli_service, snmp_parameters)
             else:
                 self._disable_snmp(cli_service, snmp_parameters)
 
-    def _disable_snmp(self, cli_service, snmp_parameters):
+    def _disable_snmp(
+        self,
+        cli_service: CliServiceImpl,
+        snmp_parameters: SNMPReadParameters | SNMPWriteParameters,
+    ) -> None:
         """Disable SNMP v2.
 
-        :param cloudshell.cli.cli_service_impl.CliServiceImpl cli_service:
-        :param cloudshell.snmp.snmp_parameters.SNMPParameters snmp_parameters:
-        :return: commands output
+        :param cli_service:
+        :param snmp_parameters:
         """
         snmp_community = snmp_parameters.snmp_community
 
@@ -142,7 +172,9 @@ class F5EnableDisableSnmpFlow(EnableDisableSnmpFlowInterface):
 
         self._disable_snmp_access(snmp_actions=snmp_actions)
 
-    def _disable_snmp_v3(self, cli_service, snmp_parameters):
+    def _disable_snmp_v3(
+        self, cli_service: CliServiceImpl, snmp_parameters: SNMPV3Parameters
+    ) -> None:
         """Disable SNMP v3.
 
         :param cloudshell.cli.cli_service_impl.CliServiceImpl cli_service:
@@ -154,7 +186,7 @@ class F5EnableDisableSnmpFlow(EnableDisableSnmpFlowInterface):
         snmp_actions.delete_snmp_user(user=snmp_parameters.snmp_user)
         self._disable_snmp_access(snmp_actions=snmp_actions)
 
-    def _disable_snmp_access(self, snmp_actions):
+    def _disable_snmp_access(self, snmp_actions: SnmpV2Actions | SnmpV3Actions) -> None:
         """Disable SNMP access.
 
         :param snmp_actions:
